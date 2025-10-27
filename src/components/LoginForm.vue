5ba5bb72f956f5cb0eeadcc2c934d2e97db37e33
Filip Kula <f.kula@1ins.pl>
2025-10-27 19:26:36 +0100

    feat: login and register view

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -8,7 +6,6 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -20,21 +17,25 @@ import {
   FormMessage,
   Form,
 } from "@/components/ui/form";
+
+function buttonForgotPassword() {
+  alert("Forgot password clicked");
+}
 </script>
 
 <template>
   <Card class="w-full max-w-sm">
-    <CardHeader>
-      <CardTitle class="text-2xl"> Login </CardTitle>
+    <CardHeader class="space-y-3 text-center">
+      <CardTitle class="text-2xl">Login</CardTitle>
       <CardDescription>
         Enter your email below to login to your account.
       </CardDescription>
     </CardHeader>
     <CardContent>
-      <Form>
-        <div class="grid gap-4">
+      <Form class="space-y-4">
+        <div class="space-y-2">
           <FormField v-slot="{ componentField }" name="email">
-            <FormItem>
+            <FormItem class="space-y-2">
               <FormLabel>Email</FormLabel>
               <FormControl>
                 <Input
@@ -49,21 +50,45 @@ import {
 
           <FormField v-slot="{ componentField }" name="password">
             <FormItem>
-              <FormLabel>Password</FormLabel>
+              <div class="flex items-center justify-between">
+                <FormLabel>Password</FormLabel>
+                <button
+                  type="button"
+                  className="text-sm text-primary hover:underline"
+                  @click="buttonForgotPassword()"
+                >
+                  Forgot password?
+                </button>
+              </div>
+
               <FormControl>
-                <Input type="password" v-bind="componentField" />
+                <Input
+                  type="password"
+                  v-bind="componentField"
+                  placeholder="Enter your password"
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
           </FormField>
 
           <Button type="submit" class="w-full"> Sign in </Button>
+
+          <div className="relative my-4">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="px-2 bg-card text-gray-500">
+                Don't have an account?
+              </span>
+            </div>
+          </div>
+          <Button type="button" variant="outline" class="w-full">
+            Create Account
+          </Button>
         </div>
       </Form>
     </CardContent>
-    <CardFooter class="text-sm">
-      Don't have an account?
-      <RouterLink to="/register" class="ml-1 underline"> Sign up </RouterLink>
-    </CardFooter>
   </Card>
 </template>
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -19,21 +16,43 @@ import {
   FormMessage,
   Form,
 } from "@/components/ui/form";
+import { Checkbox } from "./ui/checkbox";
+import { Label } from "./ui/label";
 </script>
 
 <template>
   <Card class="w-full max-w-sm">
-    <CardHeader>
-      <CardTitle class="text-2xl"> Rejestracja </CardTitle>
+    <CardHeader class="space-y-3 text-center">
+      <CardTitle class="text-2xl">Create account</CardTitle>
       <CardDescription>
-        Utwórz nowe konto, podając swój email i hasło.
+        Get started with sensor management today
       </CardDescription>
     </CardHeader>
     <CardContent>
-      <Form>
-        <div class="grid gap-4">
+      <Form class="space-y-4">
+        <div class="space-y-2">
+          <FormField v-slot="{ componentField }" name="firstName">
+            <FormItem class="space-y-2">
+              <FormLabel>First name</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="John" v-bind="componentField" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+
+          <FormField v-slot="{ componentField }" name="lastName">
+            <FormItem class="space-y-2">
+              <FormLabel>Last name</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="Doe" v-bind="componentField" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+
           <FormField v-slot="{ componentField }" name="email">
-            <FormItem>
+            <FormItem class="space-y-2">
               <FormLabel>Email</FormLabel>
               <FormControl>
                 <Input
@@ -47,32 +66,59 @@ import {
           </FormField>
 
           <FormField v-slot="{ componentField }" name="password">
-            <FormItem>
-              <FormLabel>Hasło</FormLabel>
+            <FormItem class="space-y-2">
+              <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input type="password" v-bind="componentField" />
+                <Input
+                  type="password"
+                  placeholder="********"
+                  v-bind="componentField"
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
           </FormField>
 
           <FormField v-slot="{ componentField }" name="confirmPassword">
-            <FormItem>
-              <FormLabel>Potwierdź hasło</FormLabel>
+            <FormItem class="space-y-2">
+              <FormLabel>Confirm Password</FormLabel>
               <FormControl>
-                <Input type="password" v-bind="componentField" />
+                <Input
+                  type="password"
+                  placeholder="********"
+                  v-bind="componentField"
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
           </FormField>
 
-          <Button type="submit" class="w-full mt-2"> Utwórz konto </Button>
+          <div class="flex items-start space-x-2">
+            <Checkbox id="terms" />
+            <Label
+              for="terms"
+              class="text-sm text-gray-600 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              I agree to the
+              <button type="button" class="text-primary hover:underline">
+                terms and conditions
+              </button>
+            </Label>
+          </div>
         </div>
+        <Button type="submit" class="w-full mt-2">Create account</Button>
       </Form>
+      <div className="relative my-4">
+        <div className="absolute inset-0 flex items-center">
+          <div className="w-full border-t border-gray-300"></div>
+        </div>
+        <div className="relative flex justify-center text-sm">
+          <span className="px-2 bg-card text-gray-500">
+            Already have an account?
+          </span>
+        </div>
+      </div>
+      <Button type="button" variant="outline" class="w-full"> Sign in </Button>
     </CardContent>
-    <CardFooter class="text-sm">
-      Masz już konto?
-      <RouterLink to="/login" class="ml-1 underline"> Zaloguj się </RouterLink>
-    </CardFooter>
   </Card>
 </template>
