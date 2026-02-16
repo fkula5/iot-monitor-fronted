a568ccc1a2659dee44cef89c15db49ce136f0154
Filip Kula <f.kula@1ins.pl>
2026-02-16 14:26:06 +0100

    ref: make sidebar fixed and enable scroll on main content

--- src/components/AdminPanelLayout.vue
+++ src/components/AdminPanelLayout.vue
@@ -5,14 +5,19 @@ import AdminPanelHeader from "./AdminPanelHeader.vue";
 
 <template>
   <div
-    class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]"
+    class="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] overflow-hidden"
   >
     <AdminPanelSidebar />
-    <div class="flex flex-col">
+
+    <div class="flex flex-col h-full overflow-hidden">
       <AdminPanelHeader />
 
-      <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-        <RouterView />
+      <main
+        class="flex-1 overflow-y-auto gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/20"
+      >
+        <div class="max-w-7xl mx-auto w-full">
+          <RouterView />
+        </div>
       </main>
     </div>
   </div>
